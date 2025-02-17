97634b8a4ab8f06508b8b30d61a0ec99e09e1a4c
fe-dudu <ehehwhdwhd@naver.com>
2025-02-17 20:54:58 +0900

    fix: route error

--- apps/user/components/KakaoSignInButton.tsx
+++ apps/user/components/KakaoSignInButton.tsx
@@ -1,20 +1,12 @@
 'use client';
 
-import { PATH } from '@/constants/path';
 import useSignIn from '@/hooks/useSignIn';
-import { useRouter } from 'next/navigation';
 
 export default function KakaoSignInButton() {
-  const router = useRouter();
   const { mutate, isPending } = useSignIn();
 
   return (
-    <button
-      disabled={isPending}
-      onClick={() => {
-        mutate(undefined, { onSuccess: () => router.push(PATH.PROTECTED) });
-      }}
-    >
+    <button disabled={isPending} onClick={() => mutate()}>
       Login with Kakao
     </button>
   );
