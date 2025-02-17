7bf66707d564c3e334f58740688e89e839a42e8a
fe-dudu <ehehwhdwhd@naver.com>
2025-02-17 22:00:56 +0900

    chore: export db pacakage

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -4,6 +4,8 @@ import postgres from 'postgres';
 const client = postgres(process.env.POSTGRES_URL as string);
 
 // biome-ignore lint/performance/noBarrelFile: <explanation>
-// biome-ignore lint/performance/noReExportAll: <explanation>
-export * from './schema';
+export { postTable } from './schema/post';
+export { tenantTable } from './schema/tenant';
+export { userRole, userTable } from './schema/user';
+export { lower } from './utils/lower';
 export const db = drizzle({ client });
